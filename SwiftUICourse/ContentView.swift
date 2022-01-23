e9f4431dbeb21184660cc3290aa207a7191edd2b
evilempire38 <evilempire38@gmail.com>
2022-01-23 17:58:37 +0300

    initial commit & firstPractice

--- SwiftUICourse/ContentView.swift
+++ SwiftUICourse/ContentView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+    @State private var isAcPresented = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.cyan.ignoresSafeArea()
+            VStack {
+                HStack {
+                    Text("Login:")
+                    Spacer()
+                    TextField("", text: $login)
+                        .frame(maxWidth: 150)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                HStack {
+                    Text("Password:")
+                    Spacer()
+                    SecureField("", text: $password)
+                        .frame(maxWidth: 150)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                Button("LOGIN") {
+                    isAcPresented = true
+                }
+                .alert("Logged", isPresented: $isAcPresented, actions: {
+                    Button("OK", role : .cancel){}
+                })
+                
+                .padding(.top, 50)
+                .padding(.bottom, 20)
+                .disabled(login.isEmpty || password.isEmpty)
+                
+            }.frame(maxWidth: 250)
+        }
+        }
     }
-}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+            
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
