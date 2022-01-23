e53dbf03f1759f9bc4470c2217686cf69754ab4a
evilempire38 <evilempire38@gmail.com>
2022-01-23 16:16:25 +0300

    Initial Commit

---
+++ SwiftUICourse/SwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseApp.swift
+//  SwiftUICourse
+//
+//  Created by Андрей Коноплёв on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
