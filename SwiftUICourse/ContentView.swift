8fda84ce76a488225a105ac0e374893180ff60dc
evilempire38 <evilempire38@gmail.com>
2022-01-26 17:13:48 +0300

    intermediate commit
    intermediate commit

--- SwiftUICourse/ContentView.swift
+++ SwiftUICourse/ContentView.swift
@@ -45,10 +45,47 @@ struct ContentView: View {
         }
         }
     }
+
+struct UserCellView : View {
+    let user = UserModel(name: "Fedor", surname: "Fedorov")
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            Image(systemName: "person.fill").modifier(ImageViewMod())
+            Text(user.name + " " + user.surname)
+        } .frame(width: 250, height: 50, alignment: .center)
+    }
+}
+
+struct GroupCellView : View {
+    let group = GroupModel(groupName: "Automative", groupUserCount: 45567)
+    var body: some View {
+        HStack{
+            Image(systemName: "person.3").modifier(ImageViewMod())
+            VStack {
+                Text(group.groupName)
+            }
+        }
+    }
+}
+
+struct ImageViewMod: ViewModifier {
+    func body(content: Content) -> some View {
+        return content
+            .scaledToFit()
+            .border(Color.blue)
+            .cornerRadius(15)
+            .frame(width: 30, height: 30, alignment: .leading)
+            .opacity(0.5)
+    }
+    }
+
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+            UserCellView()
+            GroupCellView()
             
         }
     }
---
+++ SwiftUICourse/UserModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserModel.swift
+//  SwiftUICourse
+//
+//  Created by Андрей Коноплёв on 26.01.2022.
+//
+
+import Foundation
+
+struct UserModel {
+    var name : String
+    var surname : String
+    
+}
+
+struct GroupModel {
+    var groupName : String
+    var groupUserCount: Int
+}
