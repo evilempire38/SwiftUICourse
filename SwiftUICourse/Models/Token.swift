de0a6638f914a2d429009a906b1206b554fb9333
evilempire38 <evilempire38@gmail.com>
2022-02-10 10:13:44 +0300

    first commt
    added some models for parce and network req

---
+++ SwiftUICourse/Models/Token.swift
@@ -0,0 +1,17 @@
+//
+//  Token.swift
+//  SwiftUICourse
+//
+//  Created by Андрей Коноплёв on 10.02.2022.
+//
+
+import Foundation
+
+class Token {
+    
+    private init(){}
+    
+    static let connecect = Token()
+    var token : String = ""
+
+}
